3789cc35181147ca2f71eb3448df659566414e4b
JhaymesBach <168040061+JhaymesBach@users.noreply.github.com>
2024-12-11 20:41:57 -0300

    Add files via upload

---
+++ InvestiPar/src/InvestiPar.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class InvestiPar {
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton cotacaobtn;
+    private JButton limparbtn;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField1;
+    private JTextField resultadobtn;
+
+    private StringBuilder expression;
+
+    public InvestiPar() {
+
+        expression = new StringBuilder();
+
+
+    }
+
+    }
+
